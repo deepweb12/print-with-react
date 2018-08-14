05ed9dc579cadfcb9fd59405bbc949f88cd5d129
raspopov.iv <raspopov.iv@K1806052>
2018-08-14 15:42:05 +0500

    build with webpack

---
+++ react-todo-app/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve('src', 'index.js'),
+    output: {
+        path: path.resolve('build'),
+        publicPath: 'build',
+        filename: 'index.js',
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            },
+            {
+                test: /\.less$/,
+                use: ['style-loader', 'css-loader', 'less-loader'],
+            },
+            {
+                test: /\.(png|svg|ttf|woff|woff2|eot)$/,
+                use: ['file-loader']
+            }
+        ]
+    },
+    resolve: {
+        modules: ['node_modules'],
+        extensions: ['.js', '.jsx'],
+    }
+};
